dfaed13f67e12b44cd7b15777a2397dd17d15881
Marcin Tustin <marcin.tustin@gmail.com>
2017-12-09 15:09:17 -0500

    Move to scoring ships using numpy matrix multiplication

--- MyBot.py
+++ MyBot.py
@@ -18,6 +18,8 @@ import random
 import math
 import copy
 import sys
+import numpy as np
+import itertools
 # GAME START
 # Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
 # this configures # logging to be compatible with halite
@@ -47,6 +49,34 @@ def planetscore(ship, planet, ship_targets, dock_attempts):
     return score
 
 
+
+PLANET_SCORING_WEIGHTS = np.array([[-70], [1000], [100], [1], [-20], [-2]])
+
+def planet_weights(ship, planets):
+    for planet in planets:
+        distance = ship.calculate_distance_between(planet)
+        is_mine = planet.is_owned() and planet.owner == ship.owner
+        is_others = planet.is_owned() and planet.owner != ship.owner
+
+        yield (int(is_mine and not planet.is_full()),
+               int(is_mine and planet.is_full()),
+               int(is_others),
+               distance,
+               int(distance < 14),
+               (0.5 - int(is_others))*planet.radius)
+
+
+def score_all_planets_for_one_ship(ship, planets, weights=PLANET_SCORING_WEIGHTS):
+    """
+    planets should be list like - position will be used to retrieve the least scoring planet
+    """
+    totalelems = len(planets) * weights.size
+    planet_features = np.fromiter(itertools.chain.from_iterable(planet_weights(ship, planets)), np.float, totalelems)
+    planet_features.shape = (len(planets), weights.size)
+    scored = np.dot(planet_features, weights)
+    best_idx = np.argmin(scored)
+    return planets[best_idx]
+
 def monotonic_deflections(seed=0, deflection_range=math.pi/32):
     deflection = seed
     while True:
@@ -75,6 +105,8 @@ while True:
     command_queue = []
     # For every ship that I control
     ships = game_map.get_me().all_ships() #[:]
+    planets = game_map.all_planets()
+    
     # random.shuffle(ships)
     for ship in ships:
         # TODO: Optimally Allocate ships between planets
@@ -88,54 +120,54 @@ while True:
         # For each planet in the game (only non-destroyed planets are included)
 
         # logging.debug("About to score planets")
-        scored_planets = sorted(game_map.all_planets(), key=lambda planet: planetscore(ship, planet, ship_targets, dock_attempts))
+        # scored_planets = sorted(game_map.all_planets(), key=lambda planet: planetscore(ship, planet, ship_targets, dock_attempts))
         # # logging.debug("Scored planets for ship {ship}: {scored_planets}".format(ship=ship, scored_planets=scored_planets))
         # logging.debug("Scored planets")
-        for n, planet in enumerate(scored_planets):
-            # logging.debug("Processing planet {}".format(n))
-            # TODO: Identify planets that are vulnerable to re-capture
-            # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
-            if (
-                    # distance is good
-                    ship.can_dock(planet) and
-                    # don't try to dock on a planet someone else owns
-                    not (planet.is_owned() and planet.owner != ship.owner) and
-                    not (planet.is_owned() and planet.owner == ship.owner and planet.is_full())):  # TODO: Don't have our own ships conflict each other
-                # We add the command by appending it to the command_queue
-                # dock_attempts[planet] = ship
-                command_queue.append(ship.dock(planet))
-                ## logging.debug("making a dock attempt right now for {ship} to {planet}".format(ship=ship, planet=planet))
-            else:
-                # TODO: What is this? Probably should be allocating ships to planets, and sending those ships to planets
-                # after recalibrating what planet they are targeted for
-                
-                # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-                # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-                # We run this navigate command each turn until we arrive to get the latest move.
-                # Here we move at half our maximum speed to better control the ships
-                # In order to execute faster we also choose to ignore ship collision calculations during navigation.
-                # This will mean that you have a higher probability of crashing into ships, but it also means you will
-                # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-                # wish to turn that option off.
+        planet = score_all_planets_for_one_ship(ship, planets)
+        # logging.debug("Processing planet {}".format(n))
+        # TODO: Identify planets that are vulnerable to re-capture
+        # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
+        if (
+                # distance is good
+                ship.can_dock(planet) and
+                # don't try to dock on a planet someone else owns
+                not (planet.is_owned() and planet.owner != ship.owner) and
+                not (planet.is_owned() and planet.owner == ship.owner and planet.is_full())):  # TODO: Don't have our own ships conflict each other
+            # We add the command by appending it to the command_queue
+            # dock_attempts[planet] = ship
+            command_queue.append(ship.dock(planet))
+            ## logging.debug("making a dock attempt right now for {ship} to {planet}".format(ship=ship, planet=planet))
+        else:
+            # TODO: What is this? Probably should be allocating ships to planets, and sending those ships to planets
+            # after recalibrating what planet they are targeted for
 
-                target_object = planet
-                if planet.is_owned() and planet.owner != ship.owner:
-                    # attack the docked ships
-                    target_object = planet.all_docked_ships()[0]
+            # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
+            # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
+            # We run this navigate command each turn until we arrive to get the latest move.
+            # Here we move at half our maximum speed to better control the ships
+            # In order to execute faster we also choose to ignore ship collision calculations during navigation.
+            # This will mean that you have a higher probability of crashing into ships, but it also means you will
+            # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
+            # wish to turn that option off.
 
-                ship_targets[ship] = target_object
-                navigate_command = ship.navigate(
-                    ship.closest_point_to(target_object),
-                    game_map,
-                    speed=int(hlt.constants.MAX_SPEED),
-                    ignore_ships=True,
-                    angle_dodges=deflections)
-                # If the move is possible, add it to the command_queue (if there are too many obstacles on the way
-                # or we are trapped (or we reached our destination!), navigate_command will return null;
-                # don't fret though, we can run the command again the next turn)
-                if navigate_command:
-                    command_queue.append(navigate_command)
-            break
+            target_object = planet
+            if planet.is_owned() and planet.owner != ship.owner:
+                # attack the docked ships
+                target_object = planet.all_docked_ships()[0]
+
+            # unused
+            #ship_targets[ship] = target_object
+            navigate_command = ship.navigate(
+                ship.closest_point_to(target_object),
+                game_map,
+                speed=int(hlt.constants.MAX_SPEED),
+                ignore_ships=False,
+                angle_dodges=deflections)
+            # If the move is possible, add it to the command_queue (if there are too many obstacles on the way
+            # or we are trapped (or we reached our destination!), navigate_command will return null;
+            # don't fret though, we can run the command again the next turn)
+            if navigate_command:
+                command_queue.append(navigate_command)
         # logging.debug("Processed all planets for ship {}".format(ship))
     # Send our set of commands to the Halite engine for this turn
     game.send_command_queue(command_queue)
